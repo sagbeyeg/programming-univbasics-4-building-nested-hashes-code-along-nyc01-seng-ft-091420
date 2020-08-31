b94f2b5fc9302a9760b714f42e08bfadc01abf91
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-31 23:09:22 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
+        name => "Lord Montague"
       :matriarch => {
         #your key/value pairs here
         },
